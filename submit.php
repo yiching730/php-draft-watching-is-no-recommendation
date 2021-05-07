113cd12267099b3e8de60099d8e3ca377dd293e1
jing <bcs109113@gm.ntcu.edu.tw>
2021-05-07 20:59:04 +0800

    membership

--- submit.php
+++ submit.php
@@ -1,11 +1,42 @@
 <?php
+$dbms = 'mysql';     //数据库类型
+$host = 'localhost'; //数据库主机名
+$dbName = 'message_board';    //使用的数据库
+$user = 'root';      //数据库连接用户名
+$pass = '';          //对应的密码
+$dsn = "$dbms:host=$host;dbname=$dbName";
 
-// $A = array(
-//     'key1' => 1,
-//     'key2' => 2,
-//     'key3' => 3
-// );
 
-// echo $A['key2'];
+try {
+    $dbh = new PDO($dsn, $user, $pass); //初始化一个PDO对象
+    echo "连接成功<br/>";
+    /*你还可以进行一次搜索操作
+    foreach ($dbh->query('SELECT * from FOO') as $row) {
+        print_r($row); //你可以用 echo($GLOBAL); 来看到这些值
+    }
+    */
+} catch (PDOException $e) {
+    die("Error!: " . $e->getMessage() . "<br/>");
+}
 
-echo 'Hello ' . $_POST['text1'];
+
+
+echo 'Hello ' . $_POST['name'];
+echo 'Hello ' . $_POST['username'];
+echo 'Hello ' . $_POST['password'];
+echo '<hr>';
+
+$name = $_POST['name'];
+$username = $_POST['username'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO `users`
+(`name`, `account`, `password`) 
+VALUES 
+('$name','$username','$password')";
+
+echo $sql;
+
+
+
+$dbh->query($sql);
